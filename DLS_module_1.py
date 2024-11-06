596ee47df337dbd8ba67863a1071d552b98d0b6a
KeerMcR <119884975+KeerMcR@users.noreply.github.com>
2024-11-06 19:29:18 +0300

    Add files via upload

---
+++ DLS_module_1.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sort()
+
+avg_score = {}
+for i in range(len(students)):
+    avg_score[students[i]] = (sum(grades[i]) / len(grades[i]))
+print(avg_score)
